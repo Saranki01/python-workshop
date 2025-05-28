5374e8ad888c5c870d3a2b48c1a94111d210a0e8
Geshan Anupama <129172291+Geshananupama@users.noreply.github.com>
2025-05-28 18:02:52 +0530

    initial commit

---
+++ Sara.py
@@ -0,0 +1,6 @@
+total=0
+for i in range(1,11):
+    sqr=i*i
+    if((sqr%4)==0):
+        total += sqr
+print("Sum of odd number from 0 to 100:",total)
---
+++ Test.py
@@ -0,0 +1,6 @@
+x=[1,2,3,4,6,4645,54,"saranki"]
+for i in x:
+    print(i)
+
+for i in range(15,10,-2):
+    print(i)
